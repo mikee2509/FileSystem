40cc4d23f62b8c0b8eb662e572b2f30ee25d3a41
Michał Sieczkowski <mikee2509@gmail.com>
2017-01-12 23:16:15 +0100

    FileSystem create and insert operations

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <bitset>
-#include <vector>
 #include "FileSystem.h"
 using namespace std;
 
@@ -14,9 +12,18 @@ void FileSystem::create(char *container) {
     if(!fs) {
         throw runtime_error("File could not be created");
     }
-    char disc[CONTAINER_SIZE*1000] = {0};
-    disc[0] |= 0b10000000; //First block is marked as taken for bitmap
-    fs.write((char*)&disc, CONTAINER_SIZE*1000*sizeof(char));
+    int nBytes = CONTAINER_SIZE * 1024;
+    char disc[nBytes] = {0};
+    fs.write((char*)&disc, nBytes * sizeof(char));
+
+    //Write END_OF_BLOCKS_TABLE at the end of blocks' table
+    fs.seekg(CONTAINER_SIZE / BLOCK_SIZE * sizeof(int));
+    int endOfBlockTable = END_OF_BLOCKS_TABLE;
+    fs.write((char*)&endOfBlockTable, sizeof(int));
+
+    Directory main(Directory::LAST_DIRECTORY_BLOCK, 0);
+    saveDirectory(main, fs);
+
     fs.close();
 }
 
@@ -31,33 +38,38 @@ void FileSystem::insert(char *container, char *fileName) {
         throw runtime_error("Error opening file");
     }
 
+    //Copy file to the container
     int fileSize = getFileSize(file);
-    File newFile(fileName, fileSize);
+    int nBlocks = getNumberOfBlocks(fileSize);
+    vector<int> fileBlocks = getEmptyBlocks(nBlocks, cfs);
+    File newFile(fileName, fileSize, fileBlocks[0]); //TODO: check for fileName length
 
-    int bitmapSize = CONTAINER_SIZE / BLOCK_SIZE / 8; //in bytes
-    char bitmap[bitmapSize]; //each char holds information about 8 blocks
-    cfs.read(bitmap, bitmapSize*sizeof(char));
-    vector<bitset<8>> bits;
-    for (int i = 0; i < bitmapSize; ++i) {
-        bits.push_back(bitset<8>(bitmap[i]));
-    }
 
-    bool isContainerEmpty = true;
-    for (int i = 0; i < 7; ++i) {
-        if(bits[0][i] == 1) isContainerEmpty = false;
-    }
-    for (int i = 1; i < bitmapSize; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            if(bits[i][j] == 1) isContainerEmpty = false;
-        }
+    int bufferSize = BLOCK_SIZE * 1024;
+    char buffer[bufferSize];
+    file.seekg(0, file.beg);
+    for(int block : fileBlocks) {
+        cout << "Copying data to block: " << block << endl;
+        file.read(buffer, bufferSize * sizeof(char));
+        seekBlock(block, cfs);
+        cfs.write(buffer, (fileSize < bufferSize ? fileSize : bufferSize) * sizeof(char));
+        fileSize -= bufferSize;
     }
 
-    if(isContainerEmpty)
-    {
-        int firstBlockPosition = bitmapSize + 1;
-        cfs.write((char*)&firstBlockPosition, sizeof(int));
+    //Update Directory information
+    Directory dir = getDirectory(cfs);
+    if(dir.numOfFiles == 0) {
+        saveFile(newFile, 0, cfs);
     }
-    //TODO: Finish this function
+    else {
+        int slot = getEmptyFileMetadataSlot(cfs);
+        saveFile(newFile, slot, cfs);
+    }
+    ++dir.numOfFiles;
+    saveDirectory(dir, cfs);
+
+    cfs.close();
+    file.close();
 }
 
 int FileSystem::getFileSize(std::fstream &fs) {
@@ -67,4 +79,78 @@ int FileSystem::getFileSize(std::fstream &fs) {
     return length;
 }
 
+void FileSystem::seekBlock(int block, std::fstream &fs) {
+    fs.seekg(block*BLOCK_SIZE*1024, fs.beg);
+}
+
+int FileSystem::getNumberOfBlocks(int size) {
+    float blocks = (float) size/(BLOCK_SIZE*1024);
+    return (int) ceil(blocks);
+}
+
+void FileSystem::seekFileMetadata(int fileIndex, std::fstream &fs) {
+    fs.seekg(BLOCK_SIZE*1024 + sizeof(Directory) + fileIndex * sizeof(File), fs.beg);
+}
+
+vector<int> FileSystem::getEmptyBlocks(int nBlocks, std::fstream &fs) {
+    fs.seekg(2*sizeof(int), fs.beg);
+    vector<int> blocks((unsigned) nBlocks);
+    for (int i = 0; i < nBlocks; ++i) {
+        do {
+            fs.read((char*)&blocks[i], sizeof(int));
+            cout << "position: " << fs.tellg()/sizeof(int)-1 << "\tval: " << blocks[i] << endl;
+        } while (blocks[i] != 0 || blocks[i] == END_OF_BLOCKS_TABLE);
+        if(blocks[i] == END_OF_BLOCKS_TABLE) {
+            throw runtime_error("Not enough space available");
+        }
+        blocks[i] = fs.tellg()/sizeof(int)-1;
+    }
+    for (int i = 0; i < nBlocks-1; ++i) {
+        seekBlocksTableElement(blocks[i], fs);
+        fs.write((char*)&blocks[i+1], sizeof(int));
+    }
+    seekBlocksTableElement(blocks[nBlocks - 1], fs);
+    int lastBlock = LAST_BLOCK_OF_FILE;
+    fs.write((char*)&lastBlock, sizeof(int));
+    return blocks;
+}
+
+void FileSystem::seekBlocksTableElement(int index, std::fstream &fs) {
+    fs.seekg(index*sizeof(int), fs.beg);
+}
+
+FileSystem::Directory FileSystem::getDirectory(std::fstream &fs) {
+    Directory dir;
+    seekBlock(1, fs);
+    fs.read((char*)&dir, sizeof(Directory));
+    return dir;
+}
+
+void FileSystem::saveDirectory(FileSystem::Directory dir, fstream &fs) {
+    seekBlock(1, fs);
+    fs.write((char*)&dir, sizeof(Directory));
+}
+
+void FileSystem::saveFile(FileSystem::File file, int index, fstream &fs) {
+    seekFileMetadata(index, fs);
+    fs.write((char*)&file, sizeof(File));
+}
+
+FileSystem::File FileSystem::getFile(int index, fstream &fs) {
+    File temp;
+    seekFileMetadata(index, fs);
+    fs.read((char*)&temp, sizeof(File));
+    return temp;
+}
+
+int FileSystem::getEmptyFileMetadataSlot(std::fstream &fs) {
+    int index = 0;
+    File temp = getFile(index, fs);
+    //TODO: check if it is not a bug- reading zeroes as File object should result in File.isActive set to 0
+    while(temp.isActive) {
+        ++index;
+        temp = getFile(index, fs);
+    }
+    return index;
+}
 
--- FileSystem.h
+++ FileSystem.h
@@ -3,33 +3,72 @@
 
 #include <fstream>
 #include <cstring>
+#include <bitset>
+#include <vector>
+#include <math.h>
 
 class FileSystem {
 public:
     const static int BLOCK_SIZE = 4; //in kilobytes
     const static int CONTAINER_SIZE = 128; //in kilobytes
+    const static int LAST_BLOCK_OF_FILE = -1;
+    const static int END_OF_BLOCKS_TABLE = -2;
     void create(char *container);
     void insert(char *container, char *fileName);
     class File;
+    class Directory;
 
     int getFileSize(std::fstream &fs); //in bytes
+
+    void seekBlock(int block, std::fstream &fs); //move file cursor to a given block
+
+    int getNumberOfBlocks(int size); //get number of blocks needed to hold *size* bytes
+
+    //Move file cursor to a file with given *fileIndex* in Directory block
+    void seekFileMetadata(int fileIndex, std::fstream &fs);
+
+    //Get index numbers of blocks that can be filled with data
+    std::vector<int> getEmptyBlocks(int nBlocks, std::fstream &fs);
+
+    //Move file cursor to *index* position in blocks' table
+    void seekBlocksTableElement(int index, std::fstream &fs);
+
+    Directory getDirectory(std::fstream &fs); //get Directory from a file stream
+
+    void saveDirectory(Directory dir, std::fstream &fs); //save Directory to a file
+
+    //Save File metadata object at position *index* in a file
+    void saveFile(File file, int index, std::fstream &fs);
+
+    File getFile(int index, std::fstream &fs); //get File metadata object of given *index* from a file stream
+
+    //Get index of an empty position in Dictionary block
+    int getEmptyFileMetadataSlot(std::fstream &fs);
+};
+
+
+class FileSystem::Directory {
+public:
+    const static int LAST_DIRECTORY_BLOCK = -3;
+    int nextDirectoryBlock; //number of the next Directory block
+    int numOfFiles; //number of files in this Directory block
+    Directory(): nextDirectoryBlock(LAST_DIRECTORY_BLOCK), numOfFiles(0) {}
+    Directory(int ndBlock, int nFiles): nextDirectoryBlock(ndBlock), numOfFiles(nFiles) {}
 };
 
 class FileSystem::File {
 public:
     char fileName[15];
-    bool active;
-    int fileSize;
-    int firstDataBlock;
-    int nextFile;
+    bool isActive; //can be replaced by a new file if false
+    int fileSize; //in bytes
+    int firstDataBlock; //index in the blocks' table
 
-    File(char *name, int size) : active(true), fileSize(size),
-                                 firstDataBlock(0), nextFile(0)
+    File() : isActive(false), fileSize(0), firstDataBlock(0) {}
+    File(char *name, int size, int block) : isActive(true), fileSize(size), firstDataBlock(block)
     {
         strncpy(fileName, name, 15);
     }
 };
-
 
 
 #endif //FILESYSTEM_FILESYSTEM_H
